fdeaff5243ef5514e9c5b66c6874851113144e18
stephenhadi <stephenhadi123@gmail.com>
2019-09-06 23:02:08 +0700

    test

--- app/src/main/java/com/example/tubes1/NumberModel.java
+++ app/src/main/java/com/example/tubes1/NumberModel.java
@@ -3,10 +3,9 @@ package com.example.tubes1;
 public class NumberModel {
     private int digit;
     private String operator;
-
+    
     public NumberModel(int digit,String operator){
         this.digit = digit;
-        
         this.operator = operator;
     }
 }
