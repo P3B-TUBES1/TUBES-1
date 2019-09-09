d452a23c859b46fedda40fad4a0a034c3eed591b
stephenhadi <stephenhadi123@gmail.com>
2019-09-09 23:56:11 +0700

    zxcvbnm

--- app/src/main/java/com/example/tubes1/MainActivity.java
+++ app/src/main/java/com/example/tubes1/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity implements IMainActivity{
     private ListView navList;
     private NavListViewAdapter navListViewAdapter;
     private ListView lstNumber;
-    private NumberListAdapter numberListAdapter;//test
+    private NumberListAdapter numberListAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
