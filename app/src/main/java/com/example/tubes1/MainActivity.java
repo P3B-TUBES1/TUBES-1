e32efc958cd7c2249b346af885145281c53281cd
stephenhadi <stephenhadi123@gmail.com>
2019-09-11 21:15:37 +0700

    Merge branch 'checkForRemote'
    # Conflicts:
    #	app/src/main/java/com/example/tubes1/MainActivity.java

--- app/src/main/java/com/example/tubes1/MainActivity.java
+++ app/src/main/java/com/example/tubes1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.tubes1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -19,7 +21,11 @@ public class MainActivity extends AppCompatActivity implements IMainActivity{
     private ListView navList;
     private NavListViewAdapter navListViewAdapter;
     private ListView lstNumber;
-    private NumberListAdapter numberListAdapter;
+    private NumberListAdapter numberListAdapter;//test
+    private FragmentManager fragmentManager;
+    private HomeFragment homeFragment;
+    private AddFragment addFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,19 +35,48 @@ public class MainActivity extends AppCompatActivity implements IMainActivity{
         this.navListViewAdapter = new NavListViewAdapter(this);
         this.navList.setAdapter(this.navListViewAdapter);
         this.lstNumber = this.findViewById(R.id.lst_number);
-        this.numberListAdapter= new NumberListAdapter(this,this.presenter);
+        this.numberListAdapter = new NumberListAdapter(this, this.presenter);
         this.toolbar = this.findViewById(R.id.action_bar);
         toolbar.setTitle("Calculator");
         this.setSupportActionBar(toolbar);
-
+        this.homeFragment = HomeFragment.newInstance();
+        this.addFragment = AddFragment.newInstance();
+        this.fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = this.fragmentManager.beginTransaction();
+        ft.add(R.id.fragment_container,this.homeFragment).commit();
     }
     @Override
-    public void updateList(List<NumberModel> list){
-
+    public void updateList(List<NumberModel> list) {
+        this.numberListAdapter.updateList(list);
     }
+
     @Override
-    public void changePage(int page){
-        Log.d("test","test");
+    public void changePage(int page) {
+        //Log.d("test", "test");
+
+        FragmentTransaction ft = this.fragmentManager.beginTransaction();
+        if (page == 1) {
+            if (this.homeFragment.isAdded()) {
+                ft.show(this.homeFragment);
+            } else {
+                ft.add(R.id.fragment_container, this.homeFragment);
+
+            }
+            if (this.addFragment.isAdded()) {
+                ft.hide(this.addFragment);
+            }
+        } else if (page == 2) {
+            if (this.addFragment.isAdded()) {
+                ft.show(this.addFragment);
+            } else {
+                ft.add(R.id.fragment_container, this.addFragment).addToBackStack(null);
+            }
+            if (this.homeFragment.isAdded()) {
+                ft.hide(this.homeFragment);
+            }
+        }
+        ft.commit();
+
     }
     @Override
     public void showResults(){
