e9cef6a30fb5378b34c024a0bf14815245785fc3
davidatz285 <55009370+davidatz285@users.noreply.github.com>
2019-09-11 20:44:59 +0700

    Add files via upload

--- app/src/main/java/com/example/tubes1/MainActivity.java
+++ app/src/main/java/com/example/tubes1/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.tubes1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Activity;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
@@ -13,13 +17,17 @@ import com.example.tubes1.Adapter.NumberListAdapter;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements IMainActivity{
+public class MainActivity extends AppCompatActivity implements IMainActivity {
     private Toolbar toolbar;
     private MainPresenter presenter;
     private ListView navList;
     private NavListViewAdapter navListViewAdapter;
     private ListView lstNumber;
     private NumberListAdapter numberListAdapter;//test
+    private FragmentManager fragmentManager;
+    private HomeFragment homeFragment;
+    private AddFragment addFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,21 +37,53 @@ public class MainActivity extends AppCompatActivity implements IMainActivity{
         this.navListViewAdapter = new NavListViewAdapter(this);
         this.navList.setAdapter(this.navListViewAdapter);
         this.lstNumber = this.findViewById(R.id.lst_number);
-        this.numberListAdapter= new NumberListAdapter(this,this.presenter);
+        this.numberListAdapter = new NumberListAdapter(this, this.presenter);
         this.toolbar = this.findViewById(R.id.action_bar);
         toolbar.setTitle("Calculator");
         this.setSupportActionBar(toolbar);
+        this.homeFragment = HomeFragment.newInstance();
+        this.addFragment = AddFragment.newInstance();
+        this.fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = this.fragmentManager.beginTransaction();
+        ft.add(R.id.fragment_container,this.homeFragment).commit();
     }
+
     @Override
-    public void updateList(List<NumberModel> list){
+    public void updateList(List<NumberModel> list) {
+        this.numberListAdapter.updateList(list);
+    }
+
+    @Override
+    public void changePage(int page) {
+        //Log.d("test", "test");
+
+        FragmentTransaction ft = this.fragmentManager.beginTransaction();
+        if (page == 1) {
+            if (this.homeFragment.isAdded()) {
+                ft.show(this.homeFragment);
+            } else {
+                ft.add(R.id.fragment_container, this.homeFragment);
+
+            }
+            if (this.addFragment.isAdded()) {
+                ft.hide(this.addFragment);
+            }
+        } else if (page == 2) {
+            if (this.addFragment.isAdded()) {
+                ft.show(this.addFragment);
+            } else {
+                ft.add(R.id.fragment_container, this.addFragment).addToBackStack(null);
+            }
+            if (this.homeFragment.isAdded()) {
+                ft.hide(this.homeFragment);
+            }
+        }
+        ft.commit();
 
     }
+
     @Override
-    public void changePage(int page){
-        Log.d("test","test");
-    }
-    @Override
-    public void showResults(){
+    public void showResults() {
 
     }
 }
--- app/src/main/java/com/example/tubes1/NumberModel.java
+++ app/src/main/java/com/example/tubes1/NumberModel.java
@@ -3,15 +3,18 @@ package com.example.tubes1;
 public class NumberModel {
     private int digit;
     private String operator;
-
+    
     public NumberModel(int digit,String operator){
         this.digit = digit;
         this.operator = operator;
     }
-    public int getOperand(){
-        return this.digit;
+
+
+    public int getOperator() {
+        return 0;
     }
-    public String getOperator(){
-        return this.operator;
+
+    public int getOperand() {
+        return 0;
     }
 }
